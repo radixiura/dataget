db28a416a47d880f420c282e2f141217a6a38d01
Radix_iura <radix_iura@protonmail.com>
2022-11-06 04:29:42 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+  "fmt"
+  "os"
+)
+
+var p = fmt.Print
+var pln = fmt.Println
+var fs = fmt.Fscan
+
+func main() {
+ pln("Welcome to dataget")
+ var surname string
+ var name string
+ var patronymic string
+ var birthdate string
+ var birthplace string
+ var email string
+ var phone string
+ var card string
+ var papers string
+ var paperscode string
+ var papersdate string
+ var snils string
+ var workplace string
+ var post string
+ var workaddress string
+ var workphone string
+ 
+ pln("Фамилия: ")
+ pln("Имя: ")
+ pln("Отчество: ")
+ pln("Дата рождения: ")
+ pln("Телефон: ")
+ pln("Почта: ")
+ pln("Серия и номер паспорта: ")
+ pln("Код отделения: ")
+ pln("Дата выдачи: ")
+ pln("Место рождения: ")
+ pln("Снилс: ")
+ pln("Место работы: ")
+ pln("Должность: ")
+ pln("Адрес работы: ")
+ pln("Телефон работодателя: ")
+
+ pln("Номер карты: ")
+ pln("Дата на карте: ")
+ pln("CVV: ")
+}
